7f4604339b7110a2b6bde31a5784080698760c79
Erin Eagleson <erineagleson@Erins-MacBook-Air.local>
2026-02-02 20:01:22 -0800

    added ProfilePage to App

--- guitarcoach/src/App.tsx
+++ guitarcoach/src/App.tsx
@@ -1,6 +1,8 @@
 import { Routes, Route } from 'react-router-dom'
 import LoginPage from './pages/LoginPage'
 import HomePage from './pages/HomePage'
+import ProfilePage from './pages/ProfilePage'
+// import QuestionnairePage from './pages/QuestionnairePage'
 import './style/App.css'
 
 function App() {
@@ -8,6 +10,7 @@ function App() {
     <Routes>
       <Route path="/login" element={<LoginPage />} />
       <Route path="/" element={<HomePage />} />
+      <Route path="/profile" element={<ProfilePage />} />
     </Routes>
   )
 }
